c8dce7218ab7e4cd95d23dc8a8206ea48a69a8b8
Sebastian Malasquez <sebasmalasquez@gmail.com>
2025-09-02 23:35:21 -0500

    Cambiando galeria

---
+++ fuxion.js
@@ -0,0 +1,7 @@
+document.getElementById("card").style.display="none";
+function mostrar(){
+    document.getElementById("card").style.display="block";
+}
+function ocultar(){
+    document.getElementById("card").style.display="none";
+}

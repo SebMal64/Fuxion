75175de2129271b7318bf18bb8341d2cb8587b5d
Sebastian Malasquez <sebasmalasquez@gmail.com>
2025-09-05 00:04:45 -0500

    lol

---
+++ data.js
@@ -0,0 +1,10 @@
+proyectos = {
+    "proyect1": {
+        "id":1,
+        "title": "drywall",
+        "description": "Este es un coinjunto de drywall hecho con pan y amllorca",
+        "tags": ["drywall", "drywall"],
+        "location": "Peru",
+        "image": "fuxion-pictures/a1.jpeg"
+    },
+}
--- fuxion.js
+++ fuxion.js
@@ -5,3 +5,11 @@ function mostrar(){
 function ocultar(){
     document.getElementById("card").style.display="none";
 }
+
+function mostrarCard() {
+    document.getElementById("card").style.display="block";
+    document.getElementById("card-head").innerText = proyectos["proyect1"].title;
+    document.getElementById("card-p").innerText = proyectos["proyect1"].description;
+    document.getElementById("location-card").innerHTML = `<img src="fuxion-pictures/marker.svg" alt="cross" class=" w-5 h-5 inline-block mr-2">ubicacion, ${proyectos["proyect1"].location}`;
+    document.getElementById("btn-card").innerHTML = `<img src="fuxion-pictures/cross.svg" alt="cross"`;
+}

e863d8a379784356ef4fdf8f249c0d83ddfc18d8
Sebastian Malasquez <sebasmalasquez@gmail.com>
2025-09-09 00:07:39 -0500

    avance en card

--- data.js
+++ data.js
@@ -1,10 +1,18 @@
-proyectos = {
-    "proyect1": {
+const proyectos = [
+    {
         "id":1,
-        "title": "drywall",
-        "description": "Este es un coinjunto de drywall hecho con pan y amllorca",
+        "title": "siubefgiubdiugsd drywall",
+        "description": "Este es un coinjunto de drywall hecho con pan y amllorca de la isugientes semansasdcas",
         "tags": ["drywall", "drywall"],
-        "location": "Peru",
+        "location": "Huaycanaa , Peru",
         "image": "fuxion-pictures/a1.jpeg"
     },
-}
+    {
+        "id":2,
+        "title": "rataa",
+        "description": "Este es un coinjunto de drywall hecho con pan y amllorca",
+        "tags": ["tatuado", "drywall", "nina"],
+        "location": "Peru",
+        "image": "fuxion-pictures/a2.jpeg"
+    },
+];
--- fuxion.js
+++ fuxion.js
@@ -6,10 +6,21 @@ function ocultar(){
     document.getElementById("card").style.display="none";
 }
 
-function mostrarCard() {
+function mostrarCard(projectId) {
+    var item = proyectos.find(p => p.id === projectId);
     document.getElementById("card").style.display="block";
-    document.getElementById("card-head").innerText = proyectos["proyect1"].title;
-    document.getElementById("card-p").innerText = proyectos["proyect1"].description;
-    document.getElementById("location-card").innerHTML = `<img src="fuxion-pictures/marker.svg" alt="cross" class=" w-5 h-5 inline-block mr-2">ubicacion, ${proyectos["proyect1"].location}`;
-    document.getElementById("btn-card").innerHTML = `<img src="fuxion-pictures/cross.svg" alt="cross"`;
+    var imgElement = document.getElementById("card-img");
+    imgElement.src = item.image;
+    imgElement.alt = item.title;
+//insert data one by one
+    document.getElementById("card-head").innerText = item.title;
+    document.getElementById("card-p").innerText = item.description;
+     let tagsHtml = "";
+    item.tags.forEach(tag => {
+        tagsHtml += `<span class="rounded bg-blue-100 p-3 inline-block mr-2 mb-2">${tag}</span>`;
+    });
+    document.getElementById("card-tags").innerHTML = tagsHtml;
+    document.getElementById("location-card").innerHTML = 
+    `<span id="location-card"><img src="fuxion-pictures/marker.svg" alt="cross" class="text-sm w-5 h-5 inline-block mr-2">${item.location}
+</span>`
 }
